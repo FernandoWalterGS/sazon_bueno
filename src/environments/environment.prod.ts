edebd9179c7147ee7edd6e2a1337137f4091ed27
waltergomezlegisoffice <waltergomezlegisoffice@gmail.com>
2025-07-05 21:15:28 -0500

    quitar pruebas17

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  API_URL: 'https://sazondelbuenobackend-production.up.railway.app',
+  API_URL: 'https://api-sazondelbueno-344011199210.southamerica-east1.run.app',
 };
